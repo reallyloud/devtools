326ed07a2f5784b74eb4d8b4df5e84e9d3aab8bf
Bark Maxim <maximalen1999@gmail.com>
2025-12-06 02:45:47 +0300

    Добавил ProgressDemo, MenteeProgress уже в проекте

---
+++ src/main/java/ru/mentee/power/ProgressDemo.java
@@ -0,0 +1,19 @@
+package ru.mentee.power;
+
+public class ProgressDemo {
+    public static void main(String[] args) {
+        // напиши new MenteeProgress( "Имя студента", 1, 6)  затем выдели и набери Ctrl + Alt + V (Windows/Linux) или Option + Command + V (macOS) выделяет выражение в переменную.
+        var progress = new MenteeProgress(
+                "Максим", // возьми значение из своего плана DVT-0
+                2,               // номер спринта
+                15                // запланированные часы на спринт
+        );
+
+        System.out.println(progress.summary());
+        if (progress.readyForSprint()) {
+            System.out.println("Status: sprint ready");
+        } else {
+            System.out.println("Status: backlog first");
+        }
+    }
+}
